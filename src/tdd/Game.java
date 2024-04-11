a3130fa50d7f923c85e97be4a0967052a3544996
H3D12004 <hedi.ben-jeddou@univ-tlse3.fr>
2024-04-11 15:01:01 +0200

    Niveau 2 : Spare

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -2,13 +2,37 @@ package tdd;
 
 public class Game {
 
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
-    void roll(int nbQuilles) {
-        score += nbQuilles;
+    void roll(int pins) {
+        rolls[currentRoll++] = pins;
     }
 
     int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
         return score;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
     }
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -40,4 +40,16 @@ public class GameTest {
         }
         assertEquals(30, game.score());
     }
+    
+    @Test
+    void test_spare() {
+        game.roll(7);
+        game.roll(3);
+        game.roll(4);
+        for (int i = 0; i < 17; i++) {
+            game.roll(0);
+        }
+        assertEquals(18, game.score()); // 7+3+4(bonus pour spare)+4 = 18
+    }
+
 }
