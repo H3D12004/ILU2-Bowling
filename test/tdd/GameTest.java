25abe03f04f618724afaf0dc48ca1355a500e87b
H3D12004 <hedi.ben-jeddou@univ-tlse3.fr>
2024-04-11 15:45:15 +0200

    FIN + quelques tests en plus

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -97,5 +97,14 @@ public class GameTest {
         }
         assertEquals(94, game.score());
     }
+    
+    @Test
+    void test_2() {
+        int[] lancers = {0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 6, 4, 7, 3, 8, 2, 9, 1, 10};
+        for (int pins : lancers) {
+            game.roll(pins);
+        }
+        assertEquals(155, game.score());
+    }
 
 }
